65380816a592624a324d5e9c22e30c653c6763db
Snappy Team <SnappyTeamMail@protonmail.com>
2021-01-23 03:07:02 -0800

    Add File For Repository
    Add File To State Snap Was Made In C-Sharp

---
+++ MadeInCSharp.cs
@@ -0,0 +1,6 @@
+// This file can be ignored!
+// This file is used to state that the project was made in C#
+// This file helps GitHub realize this and state the repository as C#
+
+// All-in-all just ignore this file completely. It is un-related to the
+// development of Snap and it's source-code.
